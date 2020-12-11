e84e1e7a968886ececf6c8720156d2db48039f95
RaccoonCoder7 <RaccoonCoder7@gmail.com>
2020-12-12 04:36:38 +0900

    - 스테이지컨트롤러 구현

---
+++ unity_project/IGW8/Assets/Test_DK/TestUIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestUIManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void OnClickNextStage()
+    {
+        Debug.Log("NextStage");
+        StageManager.Instance.LoadNextStage();
+    }
+
+    public void OnClickNextMap()
+    {
+        Debug.Log("NextMap");
+        StageManager.Instance.LoadNextMap();
+    }
+}
